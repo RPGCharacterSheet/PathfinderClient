b36eea962f1ec53c509b16f29431fd564fc414c7
Joel <jgimbel@live.com>
2016-11-14 05:13:55 -1200

    Convert to class sugar syntax

--- src/Models/Character.js
+++ src/Models/Character.js
@@ -2,35 +2,25 @@ import Enums from './Enums'
 const { Race, Size, Alignment } = Enums
 
 
-const GetAbilityModifier = (i) => Math.floor(i / 2) - 5
-
-function AddGetter({ object, key, get }) {
-    Object.defineProperty( object, key, { get })
-}
-
-export default function Character(obj) {
-    const o = Object.assign(obj, {
-        _id: obj._id['$oid'],
-        Owner: obj.Owner['$oid'],
-        Race: Race[obj.Race],
-        Size: Size[obj.Size],
-        Alignment: Alignment[obj.Alignment]
+export default class Character {
+  constructor (obj) {
+    Object.assign(this, obj, {
+      _id: obj._id['$oid'],
+      Owner: obj.Owner['$oid'],
+      Race: Race[obj.Race],
+      Size: Size[obj.Size],
+      Alignment: Alignment[obj.Alignment]
     })
+  }
 
+  GetAbilityModifier (i) { return Math.floor(i / 2) - 5 }
 
-    const getters = {
-        AbilityModifiers: function () {
-            const mods = Object.keys(this.AbilityScores).map(key => ({ [key]: GetAbilityModifier(o.AbilityScores[key]) }))
-            return Object.assign.apply(null, mods)
-        },
-        Initative: function () {
-            return this.AbilityModifiers.Dexterity
-        }
-    }
+  get AbilityModifiers () {
+    const mods = Object.keys(this.AbilityScores).map(key => ({ [key]: this.GetAbilityModifier(this.AbilityScores[key]) }))
+    return Object.assign.apply(null, mods)
+  }
 
-    Object
-        .keys(getters)
-        .forEach(getter => AddGetter({ object: o, key: getter, get: getters[getter] }))
-
-    return o
+  get Initiative () {
+    return this.AbilityModifiers.Dexterity
+  }
 }
