7614100f3631720396a41b90485159736539584d
Jim <jimkottas@nanonation.net>
2016-11-13 23:45:31 -0600

    First draft of extra getters on Character

--- src/Models/Character.js
+++ src/Models/Character.js
@@ -2,13 +2,35 @@ import Enums from './Enums'
 const { Race, Size, Alignment } = Enums
 
 
-export default function Character (obj) {
-  return {
-    ...obj,
-    _id: obj._id['$oid'],
-    Owner: obj.Owner['$oid'],
-    Race: Race[obj.Race],
-    Size: Size[obj.Size],
-    Alignment: Alignment[obj.Alignment]
-  }
+const GetAbilityModifier = (i) => Math.floor(i / 2) - 5
+
+function AddGetter({ object, key, get }) {
+    Object.defineProperty( object, key, { get })
+}
+
+export default function Character(obj) {
+    const o = Object.assign(obj, {
+        _id: obj._id['$oid'],
+        Owner: obj.Owner['$oid'],
+        Race: Race[obj.Race],
+        Size: Size[obj.Size],
+        Alignment: Alignment[obj.Alignment]
+    })
+
+
+    const getters = {
+        AbilityModifiers: function () {
+            const mods = Object.keys(this.AbilityScores).map(key => ({ [key]: GetAbilityModifier(o.AbilityScores[key]) }))
+            return Object.assign.apply(null, mods)
+        },
+        Initative: function () {
+            return this.AbilityModifier.Dexterity
+        }
+    }
+
+    Object
+        .keys(getters)
+        .forEach(getter => AddGetter({ object: o, key: getter, get: getters[getter] }))
+
+    return o
 }
