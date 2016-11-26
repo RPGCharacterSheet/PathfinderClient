5c545f33717a8cb0c3c0bcc66379f8ed28d66a62
Joel Gimbel <jgimbel@live.com>
2016-11-25 20:04:14 -1200

    Trying to make this code for the Block component look clean...

--- src/Views/Character/block.js
+++ src/Views/Character/block.js
@@ -9,27 +9,53 @@ class Content extends Component {
 
   contentToComponent (overWrite) {
     const { selected, content, character, level, editable, key } = Object.assign({}, this.props, overWrite)
+
     if (Array.isArray(content)) {
       return content.map((b, idx) => this.contentToComponent({ content: b, key: idx }))
     } else if (typeof content === 'object') {
-      return <Block {...content} character={character} level={level + 1} key={key || 0} selected={selected} />
-    } else if (typeof content === 'string') {
-      if (content[0] === '$') {
-        const value = result(character, content.slice(1))
-        let rendered = null
+      return (
+        <Block
+          {...content}
+          character={character}
+          level={level + 1}
+          key={key || 0}
+          selected={selected}
+        />
+      )
+    } else if (typeof content === 'string' && content[0] === '$') {
+      const value = result(character, content.slice(1))
+      let rendered = null
 
-        if (editable) {
-          rendered = <input key={key || 0} onChange={(event) => this.props[editable](selected, { [content.slice(1)]: event.target.value })} value={value.stat || value} />
-        } else {
-          rendered = <span>{value.stat || value}</span>
-        }
-        if (typeof value === 'object') {
-          rendered = <ToolTip placement='top' trigger={['hover']} overlay={<span>{value.description}</span>}>{rendered}</ToolTip>
-        }
-        return rendered
+      if (editable) {
+        rendered = (
+          <input
+            key={key || 0}
+            onChange={(event) => this.props[editable](selected, { [content.slice(1)]: event.target.value })}
+            value={value.stat || value}
+          />
+        )
       } else {
-        return <div key={key || 0} >{content}</div>
+        rendered = (
+          <span>
+            {value.stat || value}
+          </span>
+        )
       }
+
+      if (typeof value === 'object') {
+        rendered = (
+          <ToolTip
+            placement='top'
+            trigger={['hover']}
+            overlay={<span>{value.description}</span>}
+          >
+            {rendered}
+          </ToolTip>
+        )
+      }
+      return rendered
+    } else {
+      return <div key={key || 0} >{content}</div>
     }
   }
 
