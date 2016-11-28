98adf32ffc47f2734f95da847ea0492cd711b7c5
Joel <jgimbel@live.com>
2016-11-28 14:48:46 -0600

    Add comment

--- src/Tests/CharactersForTesting.js
+++ src/Tests/CharactersForTesting.js
@@ -5,29 +5,34 @@ import Enums from '../Models/Enums'
 const { Race, Size, Alignment } = Enums
 
 export function GetBlankCharacter () {
-    return new CharacterWithDescription(blankCharacter)
-  }
+  return new CharacterWithDescription(blankCharacter)
+}
 
 export function GetBob(){
-    let bob = this.GetBlankCharacter()
-    bob.AbilityScores.Strength = 10 
-    bob.AbilityScores.Constitution = 10 
-    bob.AbilityScores.Dexterity = 10 
-    bob.AbilityScores.Wisdom = 10 
-    bob.AbilityScores.Intelligence = 10 
-    bob.AbilityScores.Charisma = 10
-    bob.Race = Race.indexOf("Human")
-    return bob
-  }
+  let bob = this.GetBlankCharacter()
+  
+  bob.AbilityScores.Strength = 10 
+  bob.AbilityScores.Constitution = 10 
+  bob.AbilityScores.Dexterity = 10 
+  bob.AbilityScores.Wisdom = 10 
+  bob.AbilityScores.Intelligence = 10 
+  bob.AbilityScores.Charisma = 10
+
+  //This could be condensed to this
+  // bob.AbilityScores = Object.keys(bob.AbilityScores)
+  //   .reduce((obj, key) => ({ ...obj, [key]: 10 }), {})
+
+  bob.Race = Race.indexOf("Human")
+  return bob
+}
 
 export function GetLarry(){
-    let larry = this.GetBlankCharacter()
-    larry.AbilityScores.Strength = 18 
-    larry.AbilityScores.Constitution = 18 
-    larry.AbilityScores.Dexterity = 18 
-    larry.AbilityScores.Wisdom = 18 
-    larry.AbilityScores.Intelligence = 18 
-    larry.AbilityScores.Charisma = 18
-    return larry
-  }
+  let larry = this.GetBlankCharacter()
+  larry.AbilityScores.Strength = 18 
+  larry.AbilityScores.Constitution = 18 
+  larry.AbilityScores.Dexterity = 18 
+  larry.AbilityScores.Wisdom = 18 
+  larry.AbilityScores.Intelligence = 18 
+  larry.AbilityScores.Charisma = 18
+  return larry
 }
