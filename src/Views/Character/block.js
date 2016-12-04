1eb573ac4765f0352e0421c983a70deb1416130e
Joel Gimbel <jgimbel@live.com>
2016-12-03 16:26:36 -1200

    Update how we check for values

--- src/Views/Character/block.js
+++ src/Views/Character/block.js
@@ -31,13 +31,13 @@ class Content extends Component {
           <input
             key={key || 0}
             onChange={(event) => this.props[editable](selected, { [content.slice(1)]: event.target.value })}
-            value={value && value.stat || value}
+            value={(value) ? (value.stat || value) : 'empty'}
           />
         )
       } else {
         rendered = (
           <span>
-            {value && value.stat || value}
+            {(value) ? ('stat' in value) ? value.stat : value : 'empty'}
           </span>
         )
       }
